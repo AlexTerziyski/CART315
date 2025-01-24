0b083c34f6c950ea34aec61de30eb74ac1ec94c3
AlexTerziyski <alexander.terziyski@mail.concordia.ca>
2025-01-23 21:32:51 -0500

    W02: Fall Asleep In-Class Activity

---
+++ Projects/FallAsleep/Assets/Collector.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Collector : MonoBehaviour
+{
+    public float xLoc, yLoc;
+    public float speed = 0.1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        xLoc = 0;
+        yLoc = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if (Input.GetKey(KeyCode.Z))
+        {
+            Debug.Log("Left");
+            xLoc -= speed;
+
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            Debug.Log("Right");
+            xLoc += speed;
+        }
+        this.transform.position = new Vector3(xLoc, yLoc, 0);
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log(other.gameObject.name);
+        if(other.gameObject.tag == "Circle"){
+            Destroy(other.gameObject);
+        }
+    }
+}
