e337676d6a705543c68b511ef2a466e5c87ebd02
AlexTerziyski <alexander.terziyski@mail.concordia.ca>
2025-03-28 19:21:45 -0400

    Godot Chess

--- rpchess/Scenes/chess.gd
+++ rpchess/Scenes/chess.gd
@@ -20,30 +20,32 @@ const WHITE_BISHOP = preload("res://Assets/white_bishop.png")
 
 const TURN_BLACK = preload("res://Assets/turn-black.png")
 const TURN_WHITE = preload("res://Assets/turn-white.png")
+const PIECE_MOVE = preload("res://Assets/Piece_move.png")
 
 @onready var pieces: Node2D = $Pieces
 @onready var dots: Node2D = $Dots
 @onready var turn: Sprite2D = $Turn
 
-Variables
- -6 = BLACK_KING
- -5 = BLACK_QUEEN
- -4 = BLACK_ROOK
- -3 = BLACK_BISHOP
- -2 = BLACK_KNIGHT
- -1 = BLACK_PAWN
-  0 = null
- 6 = WHITE_KING
- 5 = WHITE_QUEEN
- 4 = WHITE_ROOK
- 3 = WHITE_BISHOP
- 2 = WHITE_KNIGHT
- 1 = WHITE_PAWN
+#Variables
+#-6 = BLACK_KING
+#-5 = BLACK_QUEEN
+#-4 = BLACK_ROOK
+#-3 = BLACK_BISHOP
+#-2 = BLACK_KNIGHT
+#-1 = BLACK_PAWN
+#0 = null
+#6 = WHITE_KING
+#5 = WHITE_QUEEN
+#4 = WHITE_ROOK
+#3 = WHITE_BISHOP
+#2 = WHITE_KNIGHT
+#1 = WHITE_PAWN
+
 var board : Array
 var white : bool
 var state: bool
 var moves = []
-var selected_pieces : Vector2
+var selected_piece : Vector2
 
 func _ready():
 	board.append([4, 2, 3, 5, 6, 3, 2, 4])
@@ -57,6 +59,21 @@ func _ready():
 	
 	display_board()
 	
+func _input(event):
+	if event is InputEventMouseButton && event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if is_mouse_out(): return
+			var var1 = snapped(get_global_mouse_position().x, 0) / CELL_WIDTH
+			var var2 = abs(snapped(get_global_mouse_position().y, 0)) / CELL_WIDTH
+			if !state && (white && board[var2][var1] > 0 || !white && board [var2][var1] < 0):
+				show_options()
+				selected_piece = Vector2(var2, var1)
+				state = true
+
+func is_mouse_out():
+	if get_global_mouse_position().x < 0 || get_global_mouse_position().x > 144 || get_global_mouse_position().y > 0 || get_global_mouse_position().y < -144: return true  
+	return false
+
 func display_board():
 	for i in BOARD_SIZE: 
 		for j in BOARD_SIZE:
@@ -78,4 +95,21 @@ func display_board():
 					3: holder.texture = WHITE_BISHOP
 					2: holder.texture = WHITE_KNIGHT
 					1: holder.texture = WHITE_PAWN
+			
+
+func show_options():
+	moves = get_moves()
+	if moves == []:
+		state = false
+		return
+	show_dots()
+func show_dots():
+	for i in moves:
+		var holder = TEXTURE_HOLDER.instantiate()
+		dots.add_child(holder)
+		holder.texture = PIECE_MOVE
+		holder.global_position = Vector2(i.y * CELL_WIDTH + (CELL_WIDTH/2), -i.x * CELL_WIDTH - (CELL_WIDTH/2))
+func get_moves():
+	pass
+
   
